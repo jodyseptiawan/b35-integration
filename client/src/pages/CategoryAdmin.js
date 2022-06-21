ceb493e395f9d8131a495e5e1efba71c14d3d7a2
Jedeye <jody.septiawan5@gmail.com>
2022-06-21 12:38:54 +0700

    update

--- client/src/pages/CategoryAdmin.js
+++ client/src/pages/CategoryAdmin.js
@@ -17,8 +17,13 @@ export default function CategoryAdmin() {
   document.title = 'DumbMerch | ' + title;
 
   // Create variabel for id product and confirm delete data with useState here ...
+  const [idDelete, setIdDelete] = useState(null);
+  const [confirmDelete, setConfirmDelete] = useState(null);
 
   // Create init useState & function for handle show-hide modal confirm here ...
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   let { data: categories, refetch } = useQuery('categoriesCache', async () => {
     const response = await API.get('/categories');
@@ -30,11 +35,32 @@ export default function CategoryAdmin() {
   };
 
   // Create function handle get id product & show modal confirm delete data here ...
+  const handleDelete = (id) => {
+    setIdDelete(id);
+    handleShow();
+  };
 
   // Create function for handle delete product here ...
   // If confirm is true, execute delete data
+  const deleteById = useMutation(async (id) => {
+    try {
+      await API.delete(`/category/${id}`);
+      refetch();
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
   // Call function for handle close modal and execute delete data with useEffect here ...
+  useEffect(() => {
+    if (confirmDelete) {
+      // Close modal confirm delete data
+      handleClose();
+      // execute delete data by id function
+      deleteById.mutate(idDelete);
+      setConfirmDelete(null);
+    }
+  }, [confirmDelete]);
 
   const addCategory = () => {
     navigate('/add-category');
